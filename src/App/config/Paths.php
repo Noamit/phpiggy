788c67d369708f8b964e6a555f9e5c2320490fdc
Noa Amit <noa1amit2@gmail.com>
2023-10-28 20:12:07 +0300

    till 143

---
+++ src/App/config/Paths.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Config;
+
+class Paths
+{
+    public const VIEWS = __DIR__ . "/../views";
+    // public const ROOT = __DIR__ . "/../../../";
+    // public const STORAGE_CACHE = __DIR__ . "/../../../storage/cache";
+    // public const ENTITY = __DIR__ . "/../Entity";
+    // public const SOURCE = __DIR__ . "/../../";
+    // public const STORAGE_UPLOADS = __DIR__ . "/../../../storage/uploads";
+}
