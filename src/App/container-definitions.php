3a8ee3dc9115e45c8ca0d1d0fa50984616a39fec
Noa Amit <noa1amit2@gmail.com>
2023-11-10 14:47:21 +0200

    178 include

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 use Framework\TemplateEngine;
 use App\Config\Paths;
+use App\Services\ValidatorService;
 
 return [
     // new TemplateEngine(Paths::VIEWS) is the return value of the function
     // fn () is arrow function
-    TemplateEngine::class => fn () => new TemplateEngine(Paths::VIEWS)
+    TemplateEngine::class => fn () => new TemplateEngine(Paths::VIEWS),
+    ValidatorService::class => fn () => new ValidatorService()
 ];
