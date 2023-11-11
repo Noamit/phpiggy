8d9eb6afec56c6831fc7e40c230bd2776122f052
Noa Amit <noa1amit2@gmail.com>
2023-11-11 18:44:09 +0200

    195 include

--- src/App/Middleware/SessionMiddleware.php
+++ src/App/Middleware/SessionMiddleware.php
@@ -5,15 +5,31 @@ declare(strict_types=1);
 namespace App\Middleware;
 
 use Framework\Contracts\MiddlewareInterface;
+use App\Exceptions\SessionException;
 
 class SessionMiddleware implements MiddlewareInterface
 {
     public function process(callable $next)
     {
+        
+        if (session_status() === PHP_SESSION_ACTIVE) {
+            throw new SessionException("Session already active. ");
+        }
+
+
+        //headers_sent , without session
+        // if this 2 lines is before the conditions, it is error and will catch the error :
+        // ob_end_clean();
+        // echo "hello";
+        if (headers_sent($filename, $line)) {
+            throw new SessionException(
+            "Headers already sent. Consider enabling output buffering. Data outputted from {$filename} - Line: {$line}"
+            );
+        }
         session_save_path("/Applications/XAMPP/xamppfiles/temp/");
         session_start();
-        dd($_SESSION);
 
         $next();
+        session_write_close();
     }
 }
--- src/App/views/register.php
+++ src/App/views/register.php
@@ -8,6 +8,11 @@
             <input type="email" name="email"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder="john@example.com" />
+            <?php if(array_key_exists('email', $errors)) :?>
+                <div class="bg-gray-100 mt-2 p-2 text-red-500">
+                    <?php echo $errors['email'][0]; ?>
+                </div>
+            <?php endif;?>
         </label>
         <!-- Age -->
         <label class="block">
@@ -15,6 +20,11 @@
             <input type="number" name="age"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder="" />
+                <?php if(array_key_exists('age', $errors)) :?>
+                <div class="bg-gray-100 mt-2 p-2 text-red-500">
+                    <?php echo $errors['age'][0]; ?>
+                </div>
+            <?php endif;?>
         </label>
         <!-- Country -->
         <label class="block">
@@ -26,6 +36,11 @@
                 <option value="Mexico">Mexico</option>
                 <option value="Invalid">Invalid Country</option>
             </select>
+            <?php if(array_key_exists('country', $errors)) :?>
+                <div class="bg-gray-100 mt-2 p-2 text-red-500">
+                    <?php echo $errors['country'][0]; ?>
+                </div>
+            <?php endif;?>
         </label>
         <!-- Social Media URL -->
         <label class="block">
@@ -33,6 +48,11 @@
             <input type="text" name="socialMediaURL"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder="" />
+                <?php if(array_key_exists('socialMediaURL', $errors)) :?>
+                <div class="bg-gray-100 mt-2 p-2 text-red-500">
+                    <?php echo $errors['socialMediaURL'][0]; ?>
+                </div>
+            <?php endif;?>
         </label>
         <!-- Password -->
         <label class="block">
@@ -40,6 +60,11 @@
             <input type="password" name="password"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder="" />
+                <?php if(array_key_exists('password', $errors)) :?>
+                <div class="bg-gray-100 mt-2 p-2 text-red-500">
+                    <?php echo $errors['password'][0]; ?>
+                </div>
+            <?php endif;?>
         </label>
         <!-- Confirm Password -->
         <label class="block">
@@ -47,6 +72,11 @@
             <input type="password" name="confirmPassword"
                 class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder="" />
+                <?php if(array_key_exists('confirmPassword', $errors)) :?>
+                <div class="bg-gray-100 mt-2 p-2 text-red-500">
+                    <?php echo $errors['confirmPassword'][0]; ?>
+                </div>
+            <?php endif;?>
         </label>
         <!-- Terms of Service -->
         <div class="block">
@@ -56,6 +86,11 @@
                         <input name="tos"
                             class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50"
                             type="checkbox" />
+                            <?php if(array_key_exists('tos', $errors)) :?>
+                <div class="bg-gray-100 mt-2 p-2 text-red-500">
+                    <?php echo $errors['tos'][0]; ?>
+                </div>
+            <?php endif;?>
                         <span class="ml-2">I accept the terms of service.</span>
                     </label>
                 </div>
