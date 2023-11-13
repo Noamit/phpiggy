af9976698e5aba2b84de1e6324c49f16e35333d9
Noa Amit <noa1amit2@gmail.com>
2023-11-13 16:35:19 +0200

    224 include

--- cli.php
+++ cli.php
@@ -14,4 +14,26 @@ $username = 'root';
 $password = '';
 $db = new Database($driver, $config, $username, $password);
 
-echo "Connected to Database";
+try{
+
+    $db->connection->beginTransaction();
+    $db->connection->query("INSERT INTO products VALUES(99, 'Gloves')");
+
+    $search = "Hats";
+    $query = "SELECT * FROM products WHERE name=:name";
+   
+    $stmt = $db->connection->prepare($query);
+    // $stmt->bindValue('name', $search, PDO::PARAM_STR);
+
+    $stmt->bindValue('name', 'Gloves', PDO::PARAM_STR);
+
+    $stmt->execute();
+    var_dump($stmt->fetchAll(PDO::FETCH_OBJ));
+    $db->connection->commit();
+    
+} catch (Exception $error) {
+    if($db->connection->inTransaction()) {
+        $db->connection->rollBack();
+    }
+    echo "Transaction faild!";
+}
